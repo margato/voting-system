62aef8d5f7c4a36e1fd0912cde1f1f2b311f92f5
Osvaldo Margato <15236942+margato@users.noreply.github.com>
2022-07-22 20:33:48 -0300

    (voteconsolidation) adicionar clear automatically

--- voteconsolidation/src/main/java/io/github/margato/vs/voteconsolidation/boundaries/voting/repository/CandidateRepository.java
+++ voteconsolidation/src/main/java/io/github/margato/vs/voteconsolidation/boundaries/voting/repository/CandidateRepository.java
@@ -1,6 +1,7 @@
 package io.github.margato.vs.voteconsolidation.boundaries.voting.repository;
 
 import io.github.margato.vs.voteconsolidation.boundaries.voting.entities.CandidateEntity;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +10,8 @@ import java.util.UUID;
 
 public interface CandidateRepository extends CrudRepository<CandidateEntity, UUID> {
 
-    @Query(nativeQuery = true, value = "UPDATE candidates SET votes = votes + :vote_received WHERE id = :candidate_id ")
+    @Modifying(clearAutomatically = true)
+    @Query(nativeQuery = true, value = "UPDATE candidates SET votes = votes + :vote_received WHERE id = :candidate_id")
     int updateCount(@Param("candidate_id") String candidateId, @Param("vote_received") int votesReceived);
 
 }
